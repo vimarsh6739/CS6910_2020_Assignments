b7c842e1ec038f29cb51f33592480d5297e72dbf
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2021-01-11 00:39:05 +0530

    Finished lstm

--- Assignment3/code/src/script.sh
+++ Assignment3/code/src/script.sh
@@ -2,8 +2,9 @@
 python nlp_preprocessing.py
 
 # Train models
-python train_embedding.py -m skipgram -d 100 -e 5 -cs 2 -bs 4
-python train_embedding.py -m skipgram -d 200 -e 5 -cs 2 -bs 4
+python train_embedding.py -m cbow -d 100 -e 5 -cs 2 -bs 512
+python train_embedding.py -m cbow -d 100 -e 5 -cs 2 -bs 512
+python train_embedding.py -m skipgram -d 100 -e 5 -cs 2 -bs 2048
+python train_embedding.py -m skipgram -d 200 -e 5 -cs 2 -bs 2048
 
-#Delete cleaned corpus
-rm -rf ../data
+
